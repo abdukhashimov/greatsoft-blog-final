713f56a04f0730258e246cfaa04501e08009492e
Madiyor <abdukhashimov@yandex.ru>
2020-02-11 00:42:36 +0500

    Connected comment to admin

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from comment.models import Comment
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('comment', 'parent', 'post')
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(Comment, CommentAdmin)
