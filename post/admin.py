e306e1e91ccc14a6ffb8510a59bf8238ca8cddc8
Madiyor <abdukhashimov@yandex.ru>
2020-02-11 00:24:36 +0500

    connected model to admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from post.models import Post, Category, Tag
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'thumbnail', 'content', 'tag', 'category')
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
