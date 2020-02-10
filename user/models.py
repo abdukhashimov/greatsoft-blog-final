d0a07c5d6d787945ef2c715edeb25bd0bef2ab3c
Madiyor <abdukhashimov@yandex.ru>
2020-02-10 22:50:33 +0500

    Fixed typo

--- user/models.py
+++ user/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
         user = self.model(
             email=self.normalize_email(email)
         )
-        user.set_password(password=password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
