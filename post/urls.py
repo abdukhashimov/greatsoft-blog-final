e371d550163e40c0b1063370f38f043cf53bcab7
Madiyor <abdukhashimov@yandex.ru>
2020-02-11 19:16:19 +0500

    added urls for tag, cat

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from post.views import PostViewSet
+from post.views import PostViewSet, TagView, CategoryView
 
 
 router = DefaultRouter()
@@ -9,5 +9,7 @@ router.register('posts', PostViewSet)
 app_name = 'post'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('tag/', TagView.as_view(), name='tag-list-create'),
+    path('category/', CategoryView.as_view(), name='cat-list-create'),
 ]
