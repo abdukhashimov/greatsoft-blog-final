439014cb3f1fb700eaab897ccb33962ced20e327
Madiyor <abdukhashimov@yandex.ru>
2020-02-11 08:15:15 +0500

    Created Basic View

--- post/views.py
+++ post/views.py
@@ -1,2 +1,11 @@
 from post.models import Post
-from rest_framework.permissions import AllowAny, IsAuthenticated,
+from post.serializers import PostListSerializer, PostDetailSerializer
+from post.permissions import IsAuthorOrReadOnly
+from post.pagination import PostPageNumberPagination
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostDetailSerializer
+    queryset = Post.objects.all()
