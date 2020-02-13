534052286f4de3998c8f1e3e75da70196f7ec7d3
Madiyor <abdukhashimov@yandex.ru>
2020-02-14 03:32:59 +0500

    fixed typo

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.static import serve
+from django.urls import path, include
+from django.conf.urls.static import static
 from django.contrib.staticfiles import views
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
@@ -19,5 +21,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('docs/', include_docs_urls(title='Great Soft Blog',
+                                    permission_classes=[AllowAny, ], authentication_classes=[]))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +\
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
