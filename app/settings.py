8eaf05659157262d8a2bc65fa9afed40fe3f20de
Madiyor <abdukhashimov@yandex.ru>
2020-02-14 03:54:42 +0500

    final heroku

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,3 +153,4 @@ CKEDITOR_CONFIGS = {
     },
 }
 # end of the setting
+django_heroku.settings(locals())
