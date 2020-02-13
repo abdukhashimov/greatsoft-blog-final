2931c4f37507fa87bb3cadfa9965c76e139516f6
Madiyor <abdukhashimov@yandex.ru>
2020-02-14 03:36:55 +0500

    final

--- app/settings.py
+++ app/settings.py
@@ -56,9 +56,9 @@ ROOT_URLCONF = 'app.urls'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
-
 
 TEMPLATES = [
     {
